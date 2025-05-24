0b0ca129504e0d5a5b6816142cb38e2378196818
krati-porwal <151995025+krati-porwal@users.noreply.github.com>
2025-05-24 15:44:30 +0530

    Add files via upload

---
+++ js(currency converter)/app.js
@@ -0,0 +1,67 @@
+const BASE_URL = "https://open.er-api.com/v6/latest";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn= document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const TOCurr = document.querySelector(".to select");
+const msg= document.querySelector(".msg");
+
+for(let select of dropdowns)
+ {
+    for (currCode in countryList){
+
+        let newOption = document.createElement("option");
+        newOption.innerText=currCode;
+        newOption.value = currCode;
+
+        if(select.name ==="from" && currCode ==="USD")
+        {
+            newOption.selected="selected";
+        } else if(select.name ==="TO" && currCode ==="INR")
+        {
+            newOption.selected="selected";
+        }
+        select.append(newOption);
+    }
+
+    select.addEventListener("change",(evt)=>{
+        updateFlag(evt.target);
+    });
+ }
+
+const updateFlag = (element)=>{
+      let currCode=element.value;
+      let countryCode= countryList[currCode];
+      let newSrc=`https://flagsapi.com/${countryCode}/shiny/64.png`;
+      let img = element.parentElement.querySelector("img");
+      img.src=newSrc;
+};
+
+btn.addEventListener("click", async (evt) => {
+    evt.preventDefault();
+    let amount = document.querySelector(".amount input");
+    let amtVal = amount.value;
+    if (amtVal == '' || amtVal < 1) {
+        amtVal = 1;
+        amount.value = 1;
+    }
+
+    try {
+        const URL = `${BASE_URL}/${fromCurr.value}`;
+        let response = await fetch(URL);
+        let data = await response.json();
+        
+        if (data.result === "success") {
+            let rate = data.rates[TOCurr.value];
+            let finalAmount = (amtVal * rate).toFixed(2);
+            msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${TOCurr.value}`;
+        } else {
+            msg.innerText = "Error fetching exchange rates. Please try again.";
+        }
+    } catch (error) {
+        msg.innerText = "Error: Unable to fetch exchange rates. Please try again later.";
+        console.error("Error:", error);
+    }
+});
+
+  
